c991120d211685da3e1ef151c01be336e6085e01
gepalova <gepa.arisha@yandex.ru>
2020-03-05 17:07:27 +0300

    initial commit

---
+++ Point.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace lab1
+{/// <summary>
+/// 
+/// </summary>
+    public class Point
+    {
+        public double x;
+        public double y;
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Constructor. </summary>
+        ///
+        /// <remarks>   Mi Air, 05.03.2020. </remarks>
+        ///
+        /// <param name="x">    The x coordinate. </param>
+        /// <param name="y">    The y coordinate. </param>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Addition operator. </summary>
+        ///
+        /// <remarks>   Mi Air, 05.03.2020. </remarks>
+        ///
+        /// <param name="p1">   The first value. </param>
+        /// <param name="p2">   A value to add to it. </param>
+        ///
+        /// <returns>   The result of the operation. </returns>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public static Point operator +(Point p1, Point p2)
+        {
+            return new Point(p1.x + p2.x, p1.y + p2.y);
+        }
+        public Point MovePoint(Vector v)
+        {
+            return new Point(this.x + v.x, this.y + v.y);
+        }
+        public Point OppositePoint()
+        {
+            return new Point((-1) * this.x, (-1) * this.y);
+        }
+    }
+}
